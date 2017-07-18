736e0e28703cc38bbe10c27c17d7982abd8191e6
Robin Ricard <ricard.robin@gmail.com>
2017-07-18 13:34:48 +0200

    Correctly collect exif orientation

--- src/ExifOrientationImg.js
+++ src/ExifOrientationImg.js
@@ -1,8 +1,28 @@
 /* @flow */
 import React, { Component } from 'react';
+import EXIF from 'exif-js';
 
 class ExifOrientationImg extends Component {
   props: Object;
+  state: {
+    orientation: ?number,
+  } = {
+    orientation: null,
+  };
+
+  _onImageLoaded(imageElement: HTMLImageElement) {
+    // Fix for an issue affecting exif-js: see https://github.com/exif-js/exif-js/issues/95
+    const windowImage = window.Image
+    window.Image = null;
+    
+    // Do the actual EXIF operations
+    EXIF.getData(imageElement, () => {
+      this.setState({ orientation: EXIF.getTag(imageElement, 'Orientation') })
+    });
+
+    // Re-establish the reference
+    window.Image = windowImage;
+  }
 
   render() {
     const {
@@ -12,7 +32,12 @@ class ExifOrientationImg extends Component {
     } = this.props;
 
     return (
-      <img src={src} alt={alt} {...imgProps} />
+      <img
+        onLoad={e => this._onImageLoaded(e.target)}
+        src={src}
+        alt={alt}
+        {...imgProps}
+      />
     );
   }
 }
